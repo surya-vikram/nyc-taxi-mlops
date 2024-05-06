30a7d5b981191bf9e5e9ccc956ff6d2e7d269fdf
Surya Vikram <suryavikramrajput@gmail.com>
2024-05-06 16:51:59 +0530

    Initialize project structure with Cookiecutter Data Science template (v1)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementing MLOps for NYC Taxi Trip Duration Analysis',
+    author='Surya Vikram',
+    license='MIT',
+)
